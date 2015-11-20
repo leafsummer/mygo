f68f7d4f8348ac4b8b74a6f0b8f7203b12e2bc20
leafsummer <wing.sun1987@gmail.com>
2015-11-20 10:22:56 +0800

    add to the learning files

---
+++ src/mytest/fileoutput.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "os"
+    "bufio"
+    "fmt"
+)
+
+func main() {
+    // var outputWriter *bufio.Writer
+    // var outputFile *os.File
+    // var outputError os.Error
+    // var outputString string
+
+    outputFile, outputError := os.OpenFile("output.dat", os.O_WRONLY|os.O_CREATE, 0666)
+    if outputError != nil {
+        fmt.Print("An error occurred with file opening or creation\n")
+        return
+    }
+    defer outputFile.Close()
+
+    outputWriter := bufio.NewWriter(outputFile)
+    outputString := "hello world!\n"
+
+    for i:=0; i<10; i++ {
+        outputWriter.WriteString(outputString)
+    }
+    outputWriter.Flush()
+}
---
+++ src/mytest/json.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+)
+
+type Address struct {
+    Type    string
+    City    string
+    Country string
+}
+
+type VCard struct {
+    FirstName string
+    LastName  string
+    Addresses []*Address
+    Remark    string 
+}
+
+func main() {
+    pa := &Address{"private", "Aartselaar", "Belgium"}
+    wa := &Address{"work", "Boom", "Bekgium"}
+    vc := VCard{"Jan", "Kersschot", []*Address{pa, wa}, "none"}
+    fmt.Printf("vc: %v \n", vc)
+    //JSON format
+    js, _ := json.Marshal(vc)
+    fmt.Printf("JSON format: %s \n", js)
+    //using an encoder:
+    file, _ := os.OpenFile("vcard.json", os.O_CREATE|os.O_WRONLY, 0)
+    defer file.Close()
+    enc := json.NewEncoder(file)
+    err := enc.Encode(vc)
+    if err != nil {
+        log.Println("Error in encoding json")
+    }
+
+    var vc_test VCard
+    err = json.Unmarshal(js, &vc_test)
+    if err != nil {
+        log.Println("Error in encoding json")
+    }
+    fmt.Printf("vc_test: %v \n", vc_test)
+
+    var vc_test1 VCard
+    file1, _ := os.Open("vcard.json")
+    defer file1.Close()
+    dec := json.NewDecoder(file1)
+    err = dec.Decode(&vc_test1)
+    if err != nil {
+        log.Println("Error in encoding json")
+    }
+    fmt.Printf("vc_test1: %v \n", vc_test1)
+}
